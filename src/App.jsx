b4e5726ea2b177cd1ed7c2bbd82c2a3f77a9af51
Abdelhassib <benmansourabdelhassib@gmail.com>
2026-05-31 14:23:21 +0100

    Build The Navbar For The Desktop View

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components"
+
+const App = () => {
+    return (
+        <div className="font-poppins">
+            <Navbar />
+            <div className="w-full h-0.5 bg-[#E4E7EB]" />
+        </div>
+    )
+}
+
+export default App
